fc3f07bf60e65198e8b8433b5d9849e9bb55f650
Pedro Moreira <pedromsmoreira51@gmail.com>
2022-05-23 22:20:09 +0100

    add settings management

--- cmd/shrtener/main.go
+++ cmd/shrtener/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/pedromsmoreira/shrtener/internal/shrtener/configuration"
+)
 
+func main() {
+	settings := configuration.NewSettings()
+	host := fmt.Sprintf("DB_HOST: %s", settings.Database.Host)
+	fmt.Println(host)
 }
---
+++ internal/shrtener/configuration/settings.go
@@ -0,0 +1,55 @@
+package configuration
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Settings struct {
+	Server   Server
+	Auth     Auth
+	Database Database
+}
+
+type Server struct {
+	Port int
+}
+
+type Auth struct {
+	Enabled bool
+}
+
+type Database struct {
+	Host     string
+	Username string
+	Password string
+	DbName   string
+	Enabled  bool
+}
+
+func NewSettings() *Settings {
+	viper.SetConfigName("settings")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./internal/shrtener/configuration")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error settings file: %w \n", err))
+	}
+
+	return &Settings{
+		Server: Server{
+			Port: viper.GetInt("server.port"),
+		},
+		Auth: Auth{
+			Enabled: viper.GetBool("auth.enabled"),
+		},
+		Database: Database{
+			Host:     viper.GetString("database.host"),
+			Username: viper.GetString("database.username"),
+			Password: viper.GetString("database.password"),
+			DbName:   viper.GetString("database.db_name"),
+			Enabled:  viper.GetBool("database.auth_enabled"),
+		},
+	}
+}
