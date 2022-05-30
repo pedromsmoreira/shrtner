f5d9f507cf9074dbc39f7ef1f6108a11559f7c1a
Pedro Moreira <pedromsmoreira51@gmail.com>
2022-05-30 22:41:57 +0100

    fix: created and expiration date correction

--- internal/shrtener/domain/url.go
+++ internal/shrtener/domain/url.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+const defaultUrlActiveTimeInHours time.Duration = time.Hour * 24
+
 var (
 	ErrConvertingExpirationDateToRFC3339 = errors.New("could not convert expiration date to RFC3339")
 	ErrConvertingCreatedDateToRFC3339    = errors.New("could not convert created date to RFC3339")
@@ -18,14 +20,19 @@ type Url struct {
 }
 
 func NewUrl(original string, expirationDate string) (*Url, error) {
+	createdDate := time.Now().UTC()
+	dc, err := time.Parse(time.RFC3339, createdDate.String())
+	if err != nil {
+		return nil, ErrConvertingCreatedDateToRFC3339
+	}
+
+	if expirationDate == "" {
+		expirationDate = createdDate.Add(defaultUrlActiveTimeInHours).String()
+	}
+
 	ts, err := time.Parse(time.RFC3339, expirationDate)
 	if err != nil {
 		return nil, ErrConvertingExpirationDateToRFC3339
-	}
-
-	dc, err := time.Parse(time.RFC3339, time.Now().UTC().String())
-	if err != nil {
-		return nil, ErrConvertingCreatedDateToRFC3339
 	}
 
 	return &Url{
